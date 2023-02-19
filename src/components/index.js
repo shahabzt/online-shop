31d2c74a193a2bbafb88528431dab61537aba764
shahabzt <118664973+shahabzt@users.noreply.github.com>
2023-02-20 03:01:37 +0330

    feat: open image by click

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchProduct } from "../store/product.action"
 import { ProductPaint } from './product.paint';
-import { Card, Image, Text, Badge, Button, Group, Grid, Flex,Spoiler } from '@mantine/core';
+import { Card, Image, Text, Badge, Button, Group, Grid, Flex, Spoiler, Anchor } from '@mantine/core';
 import { Loading } from './loader';
 
 
@@ -15,66 +15,70 @@ export function ShowProductData() {
         dispatch(fetchProduct())
     }, [])
 
+    
+
     return (
         <>
-        <h1>   ZARA  </h1>
+            <h1>   ZARA  </h1>
             <Grid>
 
 
-            {product.isLoading ? <Loading/> : product.error ? <h1>{product.error}</h1> :
-                product.product.map((product) => {
-                    return (
-                        // <ProductPaint  image= {product.image} description = {product.description} price = {product.price} title = {product.title} />                            
-                        <Grid.Col span={4} key={product.id}>
+                {product.isLoading ? <Loading /> : product.error ? <h1>{product.error}</h1> :
+                    product.product.map((product) => {
+                        return (
+                            // <ProductPaint  image= {product.image} description = {product.description} price = {product.price} title = {product.title} />                            
+                            <Grid.Col span={4} key={product.id}>
                                 <Flex
-                                 
-                                 justify="center"
-                                 align="center"
-                                 direction="row"
-                                 wrap="wrap"
+
+                                    justify="center"
+                                    align="center"
+                                    direction="row"
+                                    wrap="wrap"
                                 >
-                            <Card withBorder>
-                                <Card.Section style={{ weight: "500px", height: "450px" }}
-                                >
-                                        <Badge color={product.rating.rate > 3.5 ? "green" : "red"} radius="md" variant="filled" >
-                                         {product.rating.rate} / 5
-                                    </Badge>
-                                    <Image
-                                        src={product.image}
-                                        width={200}
-                                        height={200}
-                                        fit="fill"
-                                        alt="Production"
-                                    
-                                         />
+                                    <Card withBorder>
+                                        <Card.Section style={{ weight: "500px", height: "450px" }}
+                                        >
+                                            <Badge color={product.rating.rate > 3.5 ? "green" : "red"} radius="md" variant="filled" >
+                                                {product.rating.rate} / 5
+                                            </Badge>
+                                            <Anchor  href={product.image}>
+                                                <Image
+                                                    src={product.image}
+                                                    width={200}
+                                                    height={200}
+                                                    fit="fill"
+                                                    alt="Production"
+                                                />
+                                            </Anchor>
 
+                                            <Group position="apart" mt="md" mb="xs">
 
-                                    <Group position="apart" mt="md" mb="xs">
+                                            </Group>
+                                            <Text weight={500} truncate>{product.title}</Text>
+                                            <Badge color="pink" variant="light">
+                                                price:   $  {product.price}
+                                            </Badge>
+                                            <Spoiler maxHeight={20} showLabel="Show More..." hideLabel="Hide"
 
-                                    </Group>
-                                        <Text weight={500} truncate>{product.title}</Text>
-                                    <Badge color="pink" variant="light">
-                                     price:   $  {product.price}
-                                    </Badge>
-                                    <Spoiler maxHeight={20} showLabel="Show More..." hideLabel="Hide"
-                                    
-                                    >
-                                        {product.description}
-                                    </Spoiler>
-                                    
-                                    <Badge variant="gradient" radius="md"  gradient={{ from: 'teal', to: 'blue', deg: 60 }} >
-                                         stock: {product.rating.count}
-                                    </Badge>
-                                </Card.Section>
-                                    <Button variant="light" color="blue" fullWidth mt="md" mb="xs" radius="md">
-                                        Add
-                                    </Button>
-                            </Card>
-                            </Flex>
-                        </Grid.Col>
-                    );
-                })}
-        </Grid></>
+                                            >
+                                                {product.description}
+                                            </Spoiler>
+
+                                            <Badge variant="gradient" radius="md" gradient={{ from: 'teal', to: 'blue', deg: 60 }} >
+                                                stock: {product.rating.count}
+                                            </Badge>
+                                        </Card.Section>
+                                        <Anchor href={product.image}>
+                                        <Button  variant="light" color="blue" fullWidth mt="md" mb="xs" radius="md">
+                                            Add
+                                        </Button>
+                                        </Anchor>
+                                    </Card>
+                                </Flex>
+                            </Grid.Col>
+                        );
+                    })}
+            </Grid></>
     )
 
 }
