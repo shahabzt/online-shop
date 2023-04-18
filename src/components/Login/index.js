54a21b30112a3b444db0b97452556c1f5aef4717
shahabzt <118664973+shahabzt@users.noreply.github.com>
2023-04-18 18:31:44 +0430

    fix: bug fix

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -3,17 +3,27 @@ import { MenuBar } from "../menu";
 import { Left } from "./Carousel";
 import { Demo } from "./Login";
 import { Grid } from "@mantine/core";
+// import { useMediaQuery } from "@mantine/hooks";
 
 export function Login(){
+    // const mediaQuery = useMediaQuery('(min-width: 17em)')
     return (
         <>
          <MenuBar />
             <Divider m="lg"></Divider>
         <Grid h={"100vh"} style={{width:"100%"}}>
-            <Grid.Col md={5} sm={12}  style={{ backgroundColor: "#ffffff" }}>
+            <Grid.Col
+            sx={{
+                '@media (max-width: 17em)': {order: 1}
+            }}
+            md={5} sm={12} orderMd={1} orderXs={2} style={{ backgroundColor: "#ffffff" }}>
                     <Left  />
             </Grid.Col>
-            <Grid.Col md={7} sm={12}  >
+            <Grid.Col
+            sx={{
+                '@media (max-width: 17em)': {order: 1}
+            }}
+            md={7} sm={12} orderMd={2} orderXs={1} >
                     <Demo />
             </Grid.Col>
         </Grid>
